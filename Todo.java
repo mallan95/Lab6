1cca1b2ddeb1c18a42efead60f8e96b79c36e8c9
mallan95 <47227905+mallan95@users.noreply.github.com>
2019-02-26 23:48:09 -0600

    ToDo
    Also wasn't on the same path? I had to completely remake it

---
+++ Todo.java
@@ -0,0 +1,39 @@
+/*
+Lab Section_________: 12
+Last Modified_______: 2019-02-26
+OU ID_______________: 113089625
+OU EMAIL____________: matthew.m.allan-1@ou.edu@ou.edu
+
+Tasks:
+1. Read Write up and set up Todo
+2. Finalize Shape
+3. Finalize Circle
+4. Finalize Polygon
+5. Finalize EquilateralTriangle and Trapezoid
+6. Build Rectangle and Square
+7. Finalize Shape toString
+8. Finalize Shape Sorter
+9. Submit
+
+Estimated Time (minutes):
+1. 5
+2. 20
+3. 40
+4. 40
+5. 20
+6. 60
+7. 20
+8. 30
+9. 10
+
+Actual Time (minutes):
+1. 5
+2. 180
+3. 20
+4. 20
+5. 40
+6. 20
+7. 30
+8. 30
+9. 40
+*/}
